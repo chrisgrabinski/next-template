dccd945507fa002ea20afa04fac1e6f617d733f0
Chris Grabiński <chris@remote.com>
2025-04-12 13:01:08 +0100

    remove forwardref

--- src/primitives/button.tsx
+++ src/primitives/button.tsx
@@ -1,47 +1,46 @@
 "use client";
 
 import { Slot } from "@radix-ui/react-slot";
-import { ButtonHTMLAttributes, MouseEventHandler, forwardRef } from "react";
+import { ComponentProps, MouseEventHandler } from "react";
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+interface ButtonProps extends ComponentProps<"button"> {
   asChild?: boolean;
   loading?: boolean;
 }
 
-const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-  (
-    { asChild, disabled = false, loading = false, onClick, type, ...props },
-    ref,
-  ) => {
-    const Component = asChild ? Slot : "button";
+const Button = ({
+  asChild,
+  disabled = false,
+  loading = false,
+  onClick,
+  type,
+  ...props
+}: ButtonProps) => {
+  const Component = asChild ? Slot : "button";
 
-    const defaultButtonType = !asChild ? "button" : undefined;
+  const defaultButtonType = !asChild ? "button" : undefined;
 
-    const isNonInteractive = disabled || loading;
+  const isNonInteractive = disabled || loading;
 
-    const handleClick: MouseEventHandler<HTMLButtonElement> = (event) => {
-      if (isNonInteractive) {
-        event.preventDefault();
-        return;
-      }
+  const handleClick: MouseEventHandler<HTMLButtonElement> = (event) => {
+    if (isNonInteractive) {
+      event.preventDefault();
+      return;
+    }
 
-      onClick?.(event);
-    };
+    onClick?.(event);
+  };
 
-    return (
-      <Component
-        aria-busy={loading || undefined}
-        aria-disabled={disabled || undefined}
-        onClick={handleClick}
-        ref={ref}
-        type={type || defaultButtonType}
-        {...props}
-      />
-    );
-  },
-);
-
-Button.displayName = "Button";
+  return (
+    <Component
+      aria-busy={loading || undefined}
+      aria-disabled={disabled || undefined}
+      onClick={handleClick}
+      type={type || defaultButtonType}
+      {...props}
+    />
+  );
+};
 
 export { Button };
 export type { ButtonProps };
