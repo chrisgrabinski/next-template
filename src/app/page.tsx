117015190778439800a4601c8dddecb316d54e54
Chris Grabiński <c.grabinski@gmail.com>
2025-08-06 18:30:25 +0100

    removed list from root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,7 @@
-import * as DataListPrimitive from "@/primitives/data-list";
-
 export default function RootPage() {
   return (
     <div>
       <h1>Hello world</h1>
-      <DataListPrimitive.Root>
-        <DataListPrimitive.Item>
-          <DataListPrimitive.Label>Label</DataListPrimitive.Label>
-          <DataListPrimitive.Value>Value</DataListPrimitive.Value>
-        </DataListPrimitive.Item>
-        <DataListPrimitive.Sub>
-          <DataListPrimitive.SubTrigger>
-            Trigger
-            <DataListPrimitive.SubIndicator />
-          </DataListPrimitive.SubTrigger>
-          <DataListPrimitive.SubContent>Content</DataListPrimitive.SubContent>
-        </DataListPrimitive.Sub>
-      </DataListPrimitive.Root>
     </div>
   );
 }
